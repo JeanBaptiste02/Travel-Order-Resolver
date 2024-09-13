7b070d367c263cd7b53d8afeec367a595ee379dc
DAMODARANE Jean-Baptiste <91695685+JeanBaptiste02@users.noreply.github.com>
2024-09-14 00:13:47 +0200

    Update requirements.txt to align with bootstrap dependencies

--- bootstrap_project/scripts/.ipynb_checkpoints/model_evaluation-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import classification_report
-
-def evaluate_model(true_labels, pred_labels):
-    report = classification_report(true_labels, pred_labels)
-    return report
--- bootstrap_project/scripts/setup_environment.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.system('pip install spacy')
-os.system('pip install transformers')
-os.system('pip install pandas')
-os.system('pip install scikit-learn')
